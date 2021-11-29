57acb37db59647c91cad7ab26a855abe392573ed
James Woolfenden <james.woolfenden@gmail.com>
2021-11-29 09:14:54 +0000

    unit test

--- bridgecrew/resource_complex_policy_test.go
+++ bridgecrew/resource_complex_policy_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccBridgecrew_policy_basic(t *testing.T) {
+func TestAccBridgecrewComplex_policy_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: testAccProviders,
